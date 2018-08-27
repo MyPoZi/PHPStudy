45dce438f1c2bfb28d39ee6cb9a7ebc29e27b176
MyPoZi <kxx555u.u@gmail.com>
2018-08-27 18:54:42 +0900

    add perfectPHP

---
+++ perfectPHP/error_reporting.php
@@ -0,0 +1,5 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On'); // Offにするとブラウザでエラーが出力されない！！
+
+echo $aa;
---
+++ perfectPHP/sample.php
@@ -0,0 +1,9 @@
+PHP スクリプトを実行します
+
+<?php
+echo "ああ",PHP_EOL;
+echo "aa",PHP_EOL,PHP_EOL,PHP_EOL;
+$bool = true;
+echo $bool;
+?>
+PHP スクリプトおわり。
